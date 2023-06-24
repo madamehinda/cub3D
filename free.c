b98e1bf2d0c53185a3c8b8ecdbf400e139441795
Hinda FERJANI <hferjani@bess-f1r1s5.clusters.42paris.fr>
2023-06-24 16:53:19 +0200

    fix valrgind les tous normalement c'est bon

--- free.c
+++ free.c
@@ -6,7 +6,7 @@
 /*   By: hferjani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 14:57:11 by hferjani          #+#    #+#             */
-/*   Updated: 2023/06/24 16:47:56 by hferjani         ###   ########.fr       */
+/*   Updated: 2023/06/24 16:52:11 by hferjani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int	exit_game(t_data *map)
 	}
 	if (map->mlx.img)
 			mlx_destroy_image(map->mlx.mlx_ptr, map->mlx.img);
+	if (map->mlx.img_swap)
+			mlx_destroy_image(map->mlx.mlx_ptr, map->mlx.img_swap);
 	if(map->mlx.mlx_win)
 		mlx_destroy_window(map->mlx.mlx_ptr, map->mlx.mlx_win);
 	if(map->mlx.mlx_ptr)
