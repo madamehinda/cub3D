83a6c67bb0e8d44186ebbca9d349c229b707c35d
Hinda FERJANI <hferjani@bess-f1r1s5.clusters.42paris.fr>
2023-06-24 18:46:59 +0200

    fix pb du w

--- moves2.c
+++ moves2.c
@@ -6,7 +6,7 @@
 /*   By: hferjani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 18:30:39 by hferjani          #+#    #+#             */
-/*   Updated: 2023/06/24 01:36:02 by hferjani         ###   ########.fr       */
+/*   Updated: 2023/06/24 18:44:09 by hferjani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,27 @@ void	ft_forward_back(t_data *data)
 {
 	if (data->mlx.forward == 1)
 	{
-		if (data->tab[(int)(data->ray.pos.x + (data->ray.dir.x
-					* data->ray.move_speed * 2))][(int)data->ray.pos.y] != '1')
-			data->ray.pos.x += data->ray.dir.x * data->ray.move_speed;
-		if (data->tab[(int)data->ray.pos.x][(int)(data->ray.pos.y
-			+ (data->ray.dir.y * data->ray.move_speed * 2))] == '0')
-			data->ray.pos.y += data->ray.dir.y * data->ray.move_speed;
+		printf("IM IN 1\n");
+		if (data->tab[(int)(data->ray.pos.x + (data->ray.dir.x * data->ray.move_speed * 2))][(int)data->ray.pos.y] != '1')
+			{
+				data->ray.pos.x += data->ray.dir.x * data->ray.move_speed;
+				printf("IM IN FIST IF\n");
+			}
+		
+		// if (data->tab[(int)data->ray.pos.x][(int)(data->ray.pos.y + (data->ray.dir.y * data->ray.move_speed * 2))] == '0')
+		// 	{
+		// 		data->ray.pos.y += data->ray.dir.y * data->ray.move_speed;
+		// 		printf("IM IN SECOND IF\n");
+		// 	}
+		if (data->tab[(int)data->ray.pos.x][(int)(data->ray.pos.y + (data->ray.dir.y * data->ray.move_speed * 2))] != '1')
+			{
+				data->ray.pos.y += data->ray.dir.y * data->ray.move_speed;
+				printf("IM IN SECOND IF\n");
+			}
 	}
 	else if (data->mlx.back == 1)
 	{
+		
 		if (data->tab[(int)(data->ray.pos.x - (data->ray.dir.x
 					* data->ray.move_speed * 2))][(int)data->ray.pos.y] != '1')
 			data->ray.pos.x -= data->ray.dir.x * data->ray.move_speed;
